051d1b2c73eb7fb2289eddfacf75045f1f207a13
Matheus Barbosa Pereira <111500704+ZzTheusBR@users.noreply.github.com>
2022-11-16 23:36:35 +0000

    Aprendendo conexão ao banco de dados SQLite. Primeiro com uma consulta

---
+++ aula4_2022-11-16a.py
@@ -0,0 +1,22 @@
+#primeiro passo: importar a biblioteca sqlite3
+import sqlite3
+
+#segundo passo: Vamos estabelecer uma conexão com o banco de dados
+conexao = sqlite3.connect("dc_universe.db")
+
+#terceiro passo: criar um objeto do tipo cursor
+cursor = conexao.cursor()
+
+#quarto passo: Comando SQL do banco
+sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+
+#quinto passo: Executar o comando SQL no SQLlite (no cursor)
+cursor.execute(sql)
+
+#sexto passo: Exibir a consulta com todos os nomes de heróis e vilões do banco de dados
+pessoas = cursor.fetchall()
+for pessoa in pessoas:
+  print(pessoa)
+
+for pessoa in pessoas:
+  print(f"Nome: {pessoa[1]} ({pessoa[3]})")
