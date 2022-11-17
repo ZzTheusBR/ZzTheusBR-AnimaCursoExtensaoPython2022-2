38e4c1de08a34aa55e1bc1bed7098f4e1551ea52
Matheus Barbosa Pereira <111500704+ZzTheusBR@users.noreply.github.com>
2022-11-17 00:27:23 +0000

    Segundo exemplo com INSERT e adicionando um novo herói/vilão

---
+++ aula4_2022-11-16b.py
@@ -0,0 +1,17 @@
+#primeiro passo: importar a biblioteca sqlite3
+import sqlite3
+
+#segundo passo: Vamos estabelecer uma conexão com o banco de dados
+conexao = sqlite3.connect("dc_universe.db")
+
+#terceiro passo: criar um objeto do tipo cursor
+cursor = conexao.cursor()
+#quarto passo: comando para inserir um herói/vilão
+sql = "INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES (12, 'The Flash', 'Barry Allen', 'Herói(na)')"
+
+#quinto passo: Executar o comando SQL
+cursor.execute(sql)
+
+#sexto passo: Confirmar o INSERT com commit() e fechar o banco 
+conexao.commit()
+conexao.close()
